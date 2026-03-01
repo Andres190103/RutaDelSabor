483aaf4c7973a21483f5d1b28117ccb8472ca0d8
Andres Aguilar <157325686+Andres190103@users.noreply.github.com>
2026-03-01 11:20:41 -0800

    Implementacion de start page

--- core_main_RutaDelSabor/urls.py
+++ core_main_RutaDelSabor/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='start_page'),
+    
     path('admin/', admin.site.urls),
     #URL de la app foodtruck
     path('', include('foodtruck.urls')),
