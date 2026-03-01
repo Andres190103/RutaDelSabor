f8c942d146aa92c3559e5adf53e037c4916108b9
Andres190103 <aguilarandres1901@gmail.com>
2026-03-01 08:27:16 -0800

    Ajustes para subir a pythonanywhere

--- core_main_RutaDelSabor/settings.py
+++ core_main_RutaDelSabor/settings.py
@@ -124,13 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static'
 
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
