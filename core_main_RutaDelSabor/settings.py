42b4ad77c38b0ae4c133bb4d43e5659033e5dc5d
Andres Aguilar <157325686+Andres190103@users.noreply.github.com>
2026-03-29 08:10:15 -0700

    Configuracion de contenedor docker listo

--- core_main_RutaDelSabor/settings.py
+++ core_main_RutaDelSabor/settings.py
@@ -80,13 +80,15 @@ WSGI_APPLICATION = 'core_main_RutaDelSabor.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": config('ENGINE'),
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT'),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
